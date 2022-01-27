2eef03d7ab75164cfb3e4ee7c0961305e2692b6b
JustinMitchell <mitchelljm04@gmail.com>
2022-01-26 21:36:28 -0500

    commit tree e63113c97bdeef4054f0549aa4f46043671f306c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+const leastFavoriteCustomer = 'soni';
+
+customerName(); 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase(customerName);
+    
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jason'
+}
